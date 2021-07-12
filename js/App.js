35bda97a3109a8761dda2ece982daf2439485e63
Luis Gabriel <Luis.Gonzalez753@std.uni.edu.ni>
2021-07-12 16:45:41 -0600

    todo final final

--- js/App.js
+++ js/App.js
@@ -7,47 +7,47 @@ const dataListText = [
     {
       id: "six six-2",
       info: "2) Datos generales del cliente: Nombre y dirección",
-      img: './img/factura-2.png'
+      img: './img/factura-2.jpg'
     },
     {
       id: "six six-3",
       info: "3) Fecha de entrega de su factura",
-      img: './img/factura-3.png'
+      img: './img/factura-3.jpg'
     },
     {
         id: "six six-4",
         info: "4) Datos relevantes en la gestión comercial",
-        img: './img/factura-4.png'
+        img: './img/factura-4.jpg'
     },
     {
         id: "six six-5",
         info: "5) C.A.C (Centro de atención al cliente \n que gestiona el suministro",
-        img: './img/factura-5.png'
+        img: './img/factura-5.jpg'
     },
     {
         id: "six six-6",
         info: "6) Detalles de conceptos facturables acorde el pliego tarifario vigente",
-        img: './img/factura-5.1.png'
+        img: './img/factura-6.jpg'
     },
     {
         id: "six six-7",
         info: "7) Cálculos conceptos de energía",
-        img: './img/factura-6.png'
+        img: './img/factura-7.jpg'
     },
     {
         id: "six six-8",
         info: "8) Espacio para detallar los consumos e importes de energía y potencias facturados por rangos de consumo",
-        img: './img/factura-7.png'
+        img: './img/factura-8.jpg'
     },
     {
         id: "six six-9",
         info: "9) Detalle de importes adeudados incluyendo el recibo actual",
-        img: './img/factura-8.png'
+        img: './img/factura-9.jpg'
     },
     {
         id: "six six-10",
         info: "10) Importe facturado en el período actual",
-        img: './img/factura-9.png'
+        img: './img/factura-10.jpg'
     },
 ]
 
@@ -61,11 +61,9 @@ const loadData = () => {
         var element = dataListText.filter(item=>(item.id === buttonId)? item.info.toString():null);
         $('.text-facture').text(element[0].info);
 
-        $('.modal').append(`
-                            <img 
+        $('.modal').append(`<img 
                                 class="img-element-facture" 
-                                src=${element[0].img}
-                            />`)
+                                src=${element[0].img}/>`)
     })
 
     $('#modal-container').click(function(){
@@ -78,7 +76,6 @@ const loadData = () => {
     $('.btn-close').click(()=>{
         $(this).addClass('out');
         $('body').removeClass('modal-active');
-
         $('.img-element-facture').remove()
     })
 };
