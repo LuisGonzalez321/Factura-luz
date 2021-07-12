60a1c75efa9343e76a50dadd5e65496bf67ae63a
Luis Gabriel <Luis.Gonzalez753@std.uni.edu.ni>
2021-07-12 15:36:37 -0600

    prueba casi final

--- js/App.js
+++ js/App.js
@@ -6,47 +6,47 @@ const dataListText = [
     },
     {
       id: "six six-2",
-      info: "Datos generales del cliente: Nombre y dirección",
+      info: "2) Datos generales del cliente: Nombre y dirección",
       img: './img/factura-2.png'
     },
     {
       id: "six six-3",
-      info: "Fecha de entrega de su factura",
+      info: "3) Fecha de entrega de su factura",
       img: './img/factura-3.png'
     },
     {
         id: "six six-4",
-        info: "Datos relevantes en la gestión comercial",
+        info: "4) Datos relevantes en la gestión comercial",
         img: './img/factura-4.png'
     },
     {
         id: "six six-5",
-        info: "C.A.C (Centro de atención al cliente \n que gestiona el suministro",
+        info: "5) C.A.C (Centro de atención al cliente \n que gestiona el suministro",
         img: './img/factura-5.png'
     },
     {
         id: "six six-6",
-        info: "Detalles de conceptos facturables acorde el pliego tarifario vigente",
+        info: "6) Detalles de conceptos facturables acorde el pliego tarifario vigente",
         img: './img/factura-5.1.png'
     },
     {
         id: "six six-7",
-        info: "Cálculos conceptos de energía",
+        info: "7) Cálculos conceptos de energía",
         img: './img/factura-6.png'
     },
     {
         id: "six six-8",
-        info: "Espacio para detallar los consumos e importes de energía y potencias facturados por rangos de consumo",
+        info: "8) Espacio para detallar los consumos e importes de energía y potencias facturados por rangos de consumo",
         img: './img/factura-7.png'
     },
     {
         id: "six six-9",
-        info: "Detalle de importes adeudados incluyendo el recibo actual",
+        info: "9) Detalle de importes adeudados incluyendo el recibo actual",
         img: './img/factura-8.png'
     },
     {
         id: "six six-10",
-        info: "Importe facturado en el período actual",
+        info: "10) Importe facturado en el período actual",
         img: './img/factura-9.png'
     },
 ]
